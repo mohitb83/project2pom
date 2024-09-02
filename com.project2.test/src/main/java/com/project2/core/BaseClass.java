77f575f7a1b0e1314cad5f19065df6d82f66f619
Mohit Baluja <baluja.mohit7@gmail.com>
2024-09-02 16:28:34 +0530

    Updated Base Clas

--- com.project2.test/src/main/java/com/project2/core/BaseClass.java
+++ com.project2.test/src/main/java/com/project2/core/BaseClass.java
@@ -1,10 +1,70 @@
 package com.project2.core;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
 public class BaseClass {
 	
+	protected static WebDriver drv;
 	
+	File file = new File("F:\\project2pom\\com.project2.test\\src\\main\\java\\com\\project2\\utils\\config.properties");
 	
+	public Properties prop = new Properties();
 	
+	FileInputStream fis;
 	
-
+	public BaseClass(){
+		try {
+			fis = new FileInputStream(file);
+			prop.load(fis);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void browser(String browserName) {
+		if(browserName.equals(prop.getProperty("browser1"))) {
+			ChromeOptions opts = new ChromeOptions();
+			opts.setAcceptInsecureCerts(true);
+			drv = new ChromeDriver(opts);
+			drv.manage().deleteAllCookies();
+			drv.manage().window().maximize();
+			drv.get(prop.getProperty("url"));
+			drv.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
+			drv.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		}
+		else if(browserName.equals(prop.getProperty("browser2"))) {
+			FirefoxOptions opts = new FirefoxOptions();
+			opts.setAcceptInsecureCerts(true);
+			drv = new FirefoxDriver(opts);
+			drv.manage().deleteAllCookies();
+			drv.manage().window().maximize();
+			drv.get(prop.getProperty("url"));
+			drv.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
+			drv.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		}
+		else if (browserName.equals(prop.getProperty("browser2"))) {
+			EdgeOptions opts = new EdgeOptions();
+			opts.setAcceptInsecureCerts(true);
+			drv = new EdgeDriver(opts);
+			drv.manage().deleteAllCookies();
+			drv.manage().window().maximize();
+			drv.get(prop.getProperty("url"));
+			drv.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
+			drv.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		} else {
+			System.out.println("No browser found");
+		}
+		
+	}
 }
--- com.project2.test/src/test/java/com/project2/test/LoginPageTest.java
+++ com.project2.test/src/test/java/com/project2/test/LoginPageTest.java
@@ -1,5 +1,19 @@
 package com.project2.test;
 
-public class LoginPageTest {
+import org.testng.annotations.Test;
 
+import com.project2.core.BaseClass;
+
+public class LoginPageTest extends BaseClass{
+	
+	public LoginPageTest() {
+		super();
+	}
+
+	@Test
+	public void launchApp() {
+		browser("chrome");
+	}
+	
+	
 }
